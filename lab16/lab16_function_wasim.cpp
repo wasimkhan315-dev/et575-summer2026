ed4e8ac037dd59e07479aa7b022c8a9221c05709
wasimkhan315-dev <wasim.khan7326@gmail.com>
2026-06-16 23:38:45 +0000

    lab16

--- lab16/lab16_function_wasim.cpp
+++ lab16/lab16_function_wasim.cpp
@@ -14,8 +14,52 @@ void intropointer(){
     // declare pointer with initial value
     int* ptrint;
     char* ptrchar;
-    string* ptrstring;
+    string* ptrstring = &n;
 
     // check pointer info
     cout << ptrint << endl;
+
+    // initialize a pointer with variable location
+    ptrint = &num;
+    ptrchar = &sym;
+
+    // check pointers
+    cout << ptrint << endl;
+    cout << ptrchar << endl;
+    cout << ptrstring << endl;
+    
+    // get the value of a pointed variable
+    cout << *ptrint<< endl;
+    cout << *ptrchar<< endl;
+    cout << *ptrstring<< endl;
 }
+
+// example 2
+void a(string v){
+    cout << "A = " << v << endl;
+}
+void b(string& v){
+    cout <<"B = " << v << endl;
+}
+void c(string* v){
+    cout << "C = " << v << endl;
+}
+
+
+// EXERCISE B 
+void fillArray(int arr[], int size){
+      for(int i = 0; i < size; i++){
+        arr[i] == rand() % 9 + 1;
+      }
+}
+  
+int countevennumber(int arr[], int size){
+    int count = 0;
+    for(int i = 0; i < size; i++){
+        if (arr[1]%2 == 0){
+            count++;
+        }
+    }
+    return count;
+}
+
--- lab16/lab16_wasim.cpp
+++ lab16/lab16_wasim.cpp
@@ -13,6 +13,38 @@ cout << "\n --- example 1: intro to pointer\n" << endl;
 
       intropointer();
 
+cout << "\n example 2: checking different" << endl;
+string something = "Hello World!";
+a(something);
+b(something);
+c(&something);
+
+cout << "\n exercise" << endl;
+
+int* ptr; // declare pointer ( ptr holds value)
+int var = 7; // assign a value
+int foo = 21; // declare new variable ; holds 21
+ptr = &var; // places var address inside pointer
+ptr = &foo; // places foo inside pointer
+int& ref = var; // gives a new way to call 
+
+
+// EXERCISE B
+#include <cstdlib>
+#include <ctime>
+void fillArray(int arr[], int size);
+int countEvenNumbers(int arr[], int size);
+srand(time(0));
+const int SIZE = 10;
+int num[SIZE];
+fillArray(num, SIZE);
+
+cout << "Array: ";
+for(int i = 0; i <SIZE; i++){
+      cout << num[i] << " ";
+}
+cout << endl;
+cout << "Number of even numbers: " << countEvenNumbers(num, SIZE) << endl;
 
     return 0;
 }
